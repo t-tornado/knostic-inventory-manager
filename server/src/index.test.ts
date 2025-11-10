e2a09e857c25b9ec1f0c6a79e6b777339386b392
t-tornado <anthonyamp0011@gmail.com>
2025-11-10 22:26:01 +0000

    added base backend setup. fixes #2

---
+++ server/src/index.test.ts
@@ -0,0 +1,10 @@
+import request from "supertest";
+
+import { app } from ".";
+
+describe("GET /", () => {
+  it("should return 200", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+  });
+});
